59d070cef36b9faef7b69b9293de58f5f320917f
ravelli <114356826+Ghibbyy@users.noreply.github.com>
2025-04-14 20:11:41 +0200

    add Fase 2

---
+++ pl_Fase-2.py
@@ -0,0 +1,66 @@
+"""
+FASE 2: Rappresentazione di una retta sul piano cartesiano
+
+Obiettivo: Disegnare una retta con equazione y = ax + b
+"""
+
+import plotly.graph_objs as lib
+import numpy as np
+
+# Creo la figura vuota
+fig = lib.Figure()
+
+# Asse X
+fig.add_shape(
+    type="line",
+    x0=-1000, y0=0, x1=10000, y1=0,
+    line=dict(color="grey", width=2),
+)
+fig.add_annotation(
+    x=50, y=0,
+    ax=48, ay=0,
+    xref="x", yref="y",
+    axref="x", ayref="y",
+    showarrow=True,
+    arrowhead=2,
+    arrowsize=1,
+    arrowwidth=2,
+    arrowcolor="grey"
+)
+
+# Asse Y 
+fig.add_shape(
+    type="line",
+    x0=0, y0=-10000, x1=0, y1=10000,
+    line=dict(color="grey", width=2),
+)
+fig.add_annotation(
+    x=0, y=50,
+    ax=0, ay=48,
+    xref="x", yref="y",
+    axref="x", ayref="y",
+    showarrow=True,
+    arrowhead=2,
+    arrowsize=1,
+    arrowwidth=2,
+    arrowcolor="grey",
+    xanchor="center",  # Ancoraggio della freccia al centro
+    yanchor="top",  # Ancoraggio della freccia in alto
+)
+x = np.linspace(-10, 10, 100)
+y = 2 * x + 1
+fig.add_trace(lib.Scatter(x=x, y=y, mode='lines', name='y = 2x + 1'))
+
+# Layout generale
+fig.update_layout(
+    title="Fase-01, creazione assi cartesiani",
+    xaxis=dict(range=[-50, 50], zeroline=False, showgrid=True, gridcolor='lightgrey'),
+    yaxis=dict(range=[-50, 50], zeroline=False, showgrid=True, gridcolor='lightgrey'),
+    plot_bgcolor="white",  # sfondo bianco
+    margin=dict(l=50, r=50, t=50, b=150),  # spazio bianco in basso
+    autosize=True,  # Adatta automaticamente la dimensione del grafico
+    dragmode="pan",  # Abilita lo spostamento (pan) del grafico
+    hovermode="closest"  # Abilita l'interazione con il grafico
+)
+
+fig.show()
